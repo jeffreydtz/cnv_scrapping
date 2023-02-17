a97ee2313593bfa5b2045c16c9b7d58a172ad7e6
Jeffrey  Dietz <jeffrey.dietz@lbo.com.ar>
2023-02-17 14:55:08 -0300

    codigo casi terminado

--- cnv.py
+++ cnv.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 import time
+import pandas as pd
+from pandas import ExcelWriter
 
 chromedriver.install()
 
@@ -23,7 +25,7 @@ diccionario_juridica = {
 driver = start_chrome(
     "https://www.cnv.gov.ar/SitioWeb/RegistrosPublicos/Agentes")
 
-wait = WebDriverWait(driver, 100000)
+wait = WebDriverWait(driver, 100)
 
 dropdown_agents = wait.until(
     EC.presence_of_element_located((By.ID, "ListaTiposAgentes")))
@@ -38,6 +40,8 @@ button_buscar.click()
 
 
 bandera = True
+
+page = 0
 
 while bandera:
     try:
@@ -62,136 +66,201 @@ while bandera:
                 elif cont == 3:
                     diccionario_juridica["Tipo de Persona"].append(cell.text)
                 elif cont == 4:
-                    diccionario_juridica["Apellido - Razon Social"].append(cell.text)
+                    diccionario_juridica["Apellido - Razon Social"].append(
+                        cell.text)
                 elif cont == 5:
                     cont = 0
                     continue
                 cont += 1
 
-            
             set_href = set()
-                 
-            next_button = driver.find_elements_by_xpath('//a[text()="Ver mas"]')
-            
+
+            next_button = driver.find_elements_by_xpath(
+                '//a[text()="Ver mas"]')
+
             next_button[i].click()
-                
-            
-            try:    
-                button_domicilio = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id="innerheading4"]/h4/a')))
-                button_domicilio.click()
 
-                time.sleep(1)
+            try:
+                try:
+                    button_domicilio = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="innerheading4"]/h4/a')))
+                    button_domicilio.click()
 
-                tr_element = wait.until(EC.presence_of_element_located(
-                    (By.XPATH, '//*[@id="innercollapse4"]/div/div/table/tbody/tr')))
-                td_elements = tr_element.find_elements_by_xpath('./td')
+                    time.sleep(1)
 
-                diccionario_juridica_domicilio = {
-                    "Tipo": td_elements[0].text,
-                    "Direccion": td_elements[1].text,
-                    "Localidad": td_elements[2].text,
-                    "Codigo Postal": td_elements[3].text,
-                    "Provincia": td_elements[4].text,
-                    "Pais": td_elements[5].text,
-                    "Telefono": td_elements[6].text,
-                }
+                    tr_element = wait.until(EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="innercollapse4"]/div/div/table/tbody/tr')))
+                    td_elements = tr_element.find_elements_by_xpath('./td')
 
-                diccionario_juridica["Domicilio"].append(diccionario_juridica_domicilio)
-                
-                time.sleep(1)
+                    diccionario_juridica_domicilio = {
+                        "Tipo": td_elements[0].text,
+                        "Direccion": td_elements[1].text,
+                        "Localidad": td_elements[2].text,
+                        "Codigo Postal": td_elements[3].text,
+                        "Provincia": td_elements[4].text,
+                        "Pais": td_elements[5].text,
+                        "Telefono": td_elements[6].text,
+                    }
 
-                button_informacionFin = wait.until(EC.presence_of_element_located((By.ID, 'col2a')))
-                button_informacionFin.click()
+                    diccionario_juridica["Domicilio"].append(
+                        diccionario_juridica_domicilio)
 
-                time.sleep(1)
-                
-                button_publicoInv = wait.until(EC.presence_of_element_located((By.XPATH, "//a/strong[text()='INFORMACION AL PUBLICO INVERSOR']/parent::a")))
-                button_publicoInv.click()
+                except:
+                    diccionario_juridica_domicilio = {
+                        "Tipo": [],
+                        "Direccion": [],
+                        "Localidad": [],
+                        "Codigo Postal": [],
+                        "Provincia": [],
+                        "Pais": [],
+                        "Telefono": [],
+                    }
 
-                time.sleep(1)
+                    for i in range(1):
 
-                button_agentesContratos = wait.until(EC.presence_of_element_located((By.XPATH, "//a/strong[text()='Nómina de agentes con los que tenga contrato']/parent::a")))
-                button_agentesContratos.click()
-                
-                
-                td_ele = driver.find_elements_by_xpath("//td[contains(text(), 'NÓMINA DE AGENTES CON CONTRATO')]")
+                        button_domicilio = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="innerheading4"]/h4/a')))
+                        button_domicilio.click()
 
-            
-                
-                for a in td_ele:
-                    href = (a.find_element_by_xpath("./following-sibling::td/a").get_attribute("href"))
-                    set_href.add(href)
-                # navigate to its sibling td element and retrieve the href attribute of the a tag inside it
-                #href_attribute = td_ele.find_element_by_xpath("./following-sibling::td/a").get_attribute("href")
+                        time.sleep(1)
 
-                # print the href attribute value
-                #print(href_attribute)
-                diccionario_juridica["Contratos"].append(set_href)
+                        tr_element = wait.until(EC.presence_of_element_located(
+                            (By.XPATH, '//*[@id="innercollapse4"]/div/div/table/tbody/tr[{}]'.format(i+1))))
+                        td_elements = tr_element.find_elements_by_xpath('./td')
 
-                driver.back()
+                        diccionario_juridica_domicilio["Tipo"].append(
+                            td_elements[0].text)
+                        diccionario_juridica_domicilio["Direccion"].append(
+                            td_elements[1].text)
+                        diccionario_juridica_domicilio["Localidad"].append(
+                            td_elements[2].text)
+                        diccionario_juridica_domicilio["Codigo Postal"].append(
+                            td_elements[3].text)
+                        diccionario_juridica_domicilio["Provincia"].append(
+                            td_elements[4].text)
+                        diccionario_juridica_domicilio["Pais"].append(
+                            td_elements[5].text)
+                        diccionario_juridica_domicilio["Telefono"].append(
+                            td_elements[6].text)
 
-                time.sleep(5)
+                        diccionario_juridica["Domicilio"].append(
+                            diccionario_juridica_domicilio)
             except:
                 diccionario_juridica["Domicilio"].append(False)
-                
-                time.sleep(1)
 
-                button_informacionFin = wait.until(EC.presence_of_element_located((By.ID, 'col2a')))
-                button_informacionFin.click()
+            time.sleep(1)
+            try:
+                try:
 
-                time.sleep(1)
+                    button_informacionFin = wait.until(
+                        EC.presence_of_element_located((By.ID, 'col2a')))
+                    button_informacionFin.click()
 
-                button_publicoInv = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="heading9_2"]/h4/a')))
-                button_publicoInv.click()
+                    time.sleep(1)
 
-                time.sleep(1)
+                    button_publicoInv = wait.until(EC.presence_of_element_located(
+                        (By.XPATH, "//a/strong[text()='INFORMACION AL PUBLICO INVERSOR']/parent::a")))
+                    button_publicoInv.click()
 
-                button_agentesContratos = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="heading30_2"]/h4/a')))
-                button_agentesContratos.click()
-                
-                td_ele = driver.find_elements_by_xpath("//td[contains(text(), 'NÓMINA DE AGENTES CON CONTRATO')]")
+                    time.sleep(1)
 
-            
-                
-                for a in td_ele:
-                    href = (a.find_element_by_xpath("./following-sibling::td/a").get_attribute("href"))
-                    set_href.add(href)
-                # navigate to its sibling td element and retrieve the href attribute of the a tag inside it
-                #href_attribute = td_ele.find_element_by_xpath("./following-sibling::td/a").get_attribute("href")
+                    button_agentesContratos = wait.until(EC.presence_of_element_located(
+                        (By.XPATH, "//a/strong[text()='Nómina de agentes con los que tenga contrato']/parent::a")))
+                    button_agentesContratos.click()
 
-                # print the href attribute value
-                #print(href_attribute)
-                diccionario_juridica["Contratos"].append(set_href)
+                    td_ele = driver.find_elements_by_xpath(
+                        "//td[contains(text(), 'NÓMINA DE AGENTES CON CONTRATO')]")
 
+                    for a in td_ele:
+                        href = (a.find_element_by_xpath(
+                            "./following-sibling::td/a").get_attribute("href"))
+                        set_href.add(href)
+                    # navigate to its sibling td element and retrieve the href attribute of the a tag inside it
+                    #href_attribute = td_ele.find_element_by_xpath("./following-sibling::td/a").get_attribute("href")
+
+                    # print the href attribute value
+                    # print(href_attribute)
+                    if set_href == set():
+                        diccionario_juridica["Contratos"].append(False)
+                    else:
+                        diccionario_juridica["Contratos"].append(set_href)
+
+                    driver.back()
+
+                    time.sleep(5)
+                except:
+
+                    time.sleep(1)
+
+                    button_informacionFin = wait.until(
+                        EC.presence_of_element_located((By.ID, 'col2a')))
+                    button_informacionFin.click()
+
+                    time.sleep(1)
+
+                    button_publicoInv = wait.until(EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="heading9_2"]/h4/a')))
+                    button_publicoInv.click()
+
+                    time.sleep(1)
+
+                    button_agentesContratos = wait.until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="heading30_2"]/h4/a')))
+                    button_agentesContratos.click()
+
+                    td_ele = driver.find_elements_by_xpath(
+                        "//td[contains(text(), 'NÓMINA DE AGENTES CON CONTRATO')]")
+
+                    for a in td_ele:
+                        href = (a.find_element_by_xpath(
+                            "./following-sibling::td/a").get_attribute("href"))
+                        set_href.add(href)
+                    # navigate to its sibling td element and retrieve the href attribute of the a tag inside it
+                    #href_attribute = td_ele.find_element_by_xpath("./following-sibling::td/a").get_attribute("href")
+
+                    # print the href attribute value
+                    # print(href_attribute)
+                    if set_href == set():
+                        diccionario_juridica["Contratos"].append(False)
+                    else:
+                        diccionario_juridica["Contratos"].append(set_href)
+
+                    driver.back()
+                    time.sleep(5)
+
+            except:
+                diccionario_juridica["Contratos"].append(False)
                 driver.back()
+
+            for i in range(page):
+                button_siguiente = wait.until(EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="tablaagentes_next"]/a')))
+                button_siguiente.click()
                 time.sleep(5)
-            
+
             tbody = wait.until(EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="tablaagentes"]/tbody')))
+                (By.XPATH, '//*[@id="tablaagentes"]/tbody')))
 
             time.sleep(5)
 
             rows = tbody.find_elements_by_xpath(".//tr")
-        
-        button_siguiente = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="tablaagentes_next"]/a')))
+
+        button_siguiente = wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="tablaagentes_next"]/a')))
         button_siguiente.click()
-        
+
+        page = page + 1
+
         time.sleep(10)
-        
-        
+
     except:
         bandera = False
-    
-    
-    
 
-print(diccionario_juridica)
 
 driver.quit()
 
+df = pd.DataFrame(diccionario_juridica)
 
-
-
-
-
+writer = ExcelWriter('cnv_juridica.xlsx')
+df.to_excel(writer, 'Sheet1', index=False)
+writer.save()
